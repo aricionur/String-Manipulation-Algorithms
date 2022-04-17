5db0602cf4b1f7238b6322e041710ab8e916cf97
Onur ARICI <oarici@suntek.io>
2022-04-17 09:25:31 -0400

    Find the most repeated character in a string

--- StringUtils.js
+++ StringUtils.js
@@ -83,6 +83,41 @@ class StringUtils {
     /* Solution - 3 */
     // return [...new Set(string)].join("")
   }
+
+  findMostRepeatedChar(string) {
+    if (!string) return string
+
+    const charFrequencies = {}
+    let highestFreq = 0
+
+    for (const char of string) {
+      if (!charFrequencies[char]) charFrequencies[char] = 1
+      else charFrequencies[char] += 1
+
+      if (charFrequencies[char] > highestFreq) highestFreq = charFrequencies[char]
+    }
+
+    // this for loop finds first char from Freq Object. Algorithm can be extended to return other chars having same highest freq.
+    for (const [char, freq] of Object.entries(charFrequencies)) if (freq === highestFreq) return char
+
+    /* Solution - 2 */
+    // const ASCII_SIZE = 256
+    // const intArrayForCharsFreq = Array(ASCII_SIZE).fill(0)
+    // let highestFreq = 0
+    // let charHasHighestFreq = null
+
+    // for (const char of string) {
+    //   const asciiChar = char.charCodeAt()
+    //   intArrayForCharsFreq[asciiChar]++
+
+    //   if (intArrayForCharsFreq[asciiChar] > highestFreq) {
+    //     highestFreq = intArrayForCharsFreq[asciiChar]
+    //     charHasHighestFreq = char
+    //   }
+    // }
+
+    // return charHasHighestFreq
+  }
 }
 
 const stringManupilations = new StringUtils()
@@ -104,3 +139,6 @@ console.log("isRotationExist_2:", isRotationExist_2)
 
 const nonDuplicatedCharString = stringManupilations.removeDuplicatedChars("Hellooo!!...")
 console.log("nonDuplicatedCharString:", nonDuplicatedCharString)
+
+const mostRepeatedChar = stringManupilations.findMostRepeatedChar("Hellooo worlddddd!!...")
+console.log("mostRepeatedChar:", mostRepeatedChar)
